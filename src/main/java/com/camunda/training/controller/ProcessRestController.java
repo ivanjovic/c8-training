38480c7b38c22c81a09554097c8c978bd75f58c2
Ivan Jovic <ivan.jovic@camunda.com>
2022-12-14 13:03:41 +0100

    validate JsonBody when starting process instance via REST-Api

--- src/main/java/com/camunda/training/controller/ProcessRestController.java
+++ src/main/java/com/camunda/training/controller/ProcessRestController.java
@@ -5,6 +5,7 @@ import com.camunda.training.controller.dto.StartProcessRequest;
 import com.camunda.training.engine.ProcessEngineService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,7 +15,9 @@ public class ProcessRestController {
     private final ProcessEngineService engineService;
 
     @PostMapping("start")
-    public ResponseEntity<ProcessInstanceResponse> start(@RequestParam(required = false) boolean dryRun, @RequestBody StartProcessRequest request) {
+    public ResponseEntity<ProcessInstanceResponse> start(
+            @RequestParam(required = false) boolean dryRun,
+            @RequestBody @Validated StartProcessRequest request) {
         ProcessInstanceResponse response = engineService.startProcessInstance(request, dryRun);
 
         return ResponseEntity.accepted()
