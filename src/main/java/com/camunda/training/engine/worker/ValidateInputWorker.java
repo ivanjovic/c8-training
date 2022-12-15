620f202ad0df79b66ffdb714eebecd1a5785a0d8
Ivan Jovic <ivan.jovic@camunda.com>
2022-12-15 13:25:23 +0100

    throw BPMN error when input data is not valid

--- src/main/java/com/camunda/training/engine/worker/ValidateInputWorker.java
+++ src/main/java/com/camunda/training/engine/worker/ValidateInputWorker.java
@@ -4,19 +4,36 @@ import com.camunda.training.engine.variables.InputData;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
 import io.camunda.zeebe.spring.client.annotation.VariablesAsType;
+import io.camunda.zeebe.spring.client.exception.ZeebeBpmnError;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import javax.validation.Valid;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class ValidateInputWorker {
 
+    private final Validator validator;
+
     @JobWorker(type = "validateInput")
-    public void execute(final ActivatedJob job, @VariablesAsType @Valid InputData input) {
-        log.info("Passing validateInput");
+    public void execute(final ActivatedJob job, @VariablesAsType InputData input) {
+        Set<ConstraintViolation<InputData>> violations = validator.validate(input);
+        if (!violations.isEmpty()) {
+            String errorMessage = formatViolations(violations);
+            log.error(errorMessage);
+            throw new ZeebeBpmnError("INVALID_INPUT_ERROR", errorMessage);
+        }
+    }
+
+    private String formatViolations(Set<ConstraintViolation<InputData>> violations) {
+        return "Invalid input-data provided: " + violations.stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.joining(","));
     }
 }
