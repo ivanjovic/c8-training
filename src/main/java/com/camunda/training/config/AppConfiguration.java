b542e5bdac7a3a1613924dcd81de3b56916bf011
Ivan Jovic <ivan.jovic@camunda.com>
2022-12-28 10:48:08 +0100

    remove bean creation of OperateClient, because - it relies on providing the Auth-Object during it's build-method which breaks the app when operate is not reachable or auth cannot be done - there is no way to split client and authentication for operate because the client relies on the auth object not to be null during it's build method - therefore the client will be created on demand within the scope it it really needed to not break the app during startup just because one component has potential problems

--- src/main/java/com/camunda/training/config/AppConfiguration.java
+++ src/main/java/com/camunda/training/config/AppConfiguration.java
@@ -1,10 +1,6 @@
 package com.camunda.training.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.camunda.operate.CamundaOperateClient;
-import io.camunda.operate.auth.AuthInterface;
-import io.camunda.operate.auth.SaasAuthentication;
-import io.camunda.operate.exception.OperateException;
 import io.camunda.zeebe.client.api.JsonMapper;
 import io.camunda.zeebe.client.impl.ZeebeObjectMapper;
 import org.springframework.context.annotation.Bean;
@@ -16,20 +12,5 @@ public class AppConfiguration {
     @Bean
     public JsonMapper jsonMapper(final ObjectMapper objectMapper) {
         return new ZeebeObjectMapper(objectMapper);
-    }
-
-    @Bean
-    public CamundaOperateClient operateClient(OperateProperties properties) throws OperateException {
-        AuthInterface authentication = new SaasAuthentication(
-                properties.getOauthUrl(),
-                properties.getTokenAudience(),
-                properties.getClientId(),
-                properties.getClientSecret()
-        );
-
-        return new CamundaOperateClient.Builder()
-                .operateUrl(properties.getBaseUrl())
-                .authentication(authentication)
-                .build();
     }
 }
--- src/main/java/com/camunda/training/engine/operate/OperateService.java
+++ src/main/java/com/camunda/training/engine/operate/OperateService.java
@@ -1,8 +1,11 @@
 package com.camunda.training.engine.operate;
 
+import com.camunda.training.config.OperateProperties;
 import com.camunda.training.exception.OperateRuntimeException;
 import com.camunda.training.exception.ProcessDefinitionNotDeployedException;
 import io.camunda.operate.CamundaOperateClient;
+import io.camunda.operate.auth.AuthInterface;
+import io.camunda.operate.auth.SaasAuthentication;
 import io.camunda.operate.dto.ProcessDefinition;
 import io.camunda.operate.exception.OperateException;
 import io.camunda.operate.search.ProcessDefinitionFilter;
@@ -13,19 +16,33 @@ import lombok.RequiredArgsConstructor;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OperateService {
 
-    private final CamundaOperateClient client;
+    private final OperateProperties properties;
+
+    private AuthInterface authentication;
+
+    @PostConstruct
+    void init() {
+        authentication = new SaasAuthentication(
+                properties.getOauthUrl(),
+                properties.getTokenAudience(),
+                properties.getClientId(),
+                properties.getClientSecret()
+        );
+    }
 
     public ProcessDefinition getLastestProcessDefinition(String bpmnProcessId) {
 
         // try-catch needed because signature of build methods has a checked exception
         // remove when fixed within the lib, should only wrap the http call itself
         try {
+            CamundaOperateClient client = getClient();
             SearchQuery processDefinitionFilter = new SearchQuery.Builder()
                     .withFilter(
                             new ProcessDefinitionFilter.Builder()
@@ -50,5 +67,12 @@ public class OperateService {
         } catch (OperateException ex) {
             throw new OperateRuntimeException(ex);
         }
+    }
+
+    private CamundaOperateClient getClient() throws OperateException {
+        return new CamundaOperateClient.Builder()
+                .operateUrl(properties.getBaseUrl())
+                .authentication(authentication)
+                .build();
     }
 }
